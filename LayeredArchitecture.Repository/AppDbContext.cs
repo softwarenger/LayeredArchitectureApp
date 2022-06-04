8f4a2e854b836d6fa2b5aeaf7e27699dc4b6a78f
ismailunlu <smlnl@outlook.com>
2022-06-04 19:32:52 +0300

    Repo İşlemleri

---
+++ LayeredArchitecture.Repository/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using LayeredArchitecture.Core;
+using LayeredArchitecture.Repository.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace LayeredArchitecture.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //Tek Tek Eklemek istersek alt satırdaki gibi de  yapabiliriz.
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ LayeredArchitecture.Repository/Configurations/ProductConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using LayeredArchitecture.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitecture.Repository.Configurations
+{
+    class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(250);
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(6,2)");
+            builder.ToTable("Products");
+
+
+
+            builder.HasOne(x => x.Category).WithMany(x => x.Products).HasForeignKey(x => x.CategoryId);
+
+        }
+    }
+}
