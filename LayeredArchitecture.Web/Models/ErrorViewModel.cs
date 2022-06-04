d61ebb1d344c4a51dd4a26fef37301412126073b
ismailunlu <smlnl@outlook.com>
2022-06-04 03:53:27 +0300

    Add project files.

---
+++ LayeredArchitecture.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LayeredArchitecture.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
